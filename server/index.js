a2ed62c15516b91fa98e31ed4f89b993f3e4c61e
RishabJha134 <rishabjha134@gmail.com>
2024-11-23 21:24:39 +0530

    fix url issue

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "https://social-gram-theta.vercel.app",
-    "https://social-gram-social-media-app.vercel.app/",
+    "https://social-gram-social-media-app.vercel.app",
   ], // Allowed frontend URLs
   methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
