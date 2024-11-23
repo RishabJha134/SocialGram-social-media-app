de92a912ef7d197a130ef5940db4d64574f0db6f
RishabJha134 <rishabjha134@gmail.com>
2024-11-23 21:09:01 +0530

    fix cors error

--- server/index.js
+++ server/index.js
@@ -13,7 +13,11 @@ app.use(cookieParser());
 
 // CORS Configuration
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://social-gram-theta.vercel.app"], // Allowed frontend URLs
+  origin: [
+    "http://localhost:5173",
+    "https://social-gram-theta.vercel.app",
+    "https://social-gram-social-media-app.vercel.app/",
+  ], // Allowed frontend URLs
   methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
   credentials: true, // Enable cookies/auth headers
